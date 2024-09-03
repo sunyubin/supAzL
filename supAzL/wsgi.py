be1763c829235bfacb95c960b10993e8e5cc6430
Darius <1109028480@qq.com>
2024-09-03 18:28:07 +0800

    首次提交

---
+++ supAzL/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for supAzL project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'supAzL.settings')
+
+application = get_wsgi_application()
